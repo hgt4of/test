8b72779139ddb9ad961616a446deebd2861eb354
hgt4of <tree.bree@mail.ru>
2026-04-09 02:56:37 +0300

    save

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,13 +1,30 @@
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SearchTests {
     @Test
     void successfulSearchTest() {
-        open("https://www.google.com/");
-        $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://selenide.org"));
+        open("https://www.automationexercise.com/");
+        $("a[href='/login']").click();
+        $("[data-qa='signup-name']").setValue("Test User");
+        $("[data-qa='signup-email']").setValue("testuser_" + System.currentTimeMillis() + "@test.com");
+        $("[data-qa='signup-button']").click();
+        $("[data-qa='password']").setValue("Password123");
+        $("[data-qa='days']").selectOption("15");
+        $("[data-qa='months']").selectOption("March");
+        $("[data-qa='years']").selectOption("1990");
+        $("[data-qa='first_name']").setValue("Test");
+        $("[data-qa='last_name']").setValue("User");
+        $("[data-qa='address']").setValue("Test Street 1");
+        $("[data-qa='country']").selectOption("United States");
+        $("[data-qa='state']").setValue("Test State");
+        $("[data-qa='city']").setValue("Test City");
+        $("[data-qa='zipcode']").setValue("12345");
+        $("[data-qa='mobile_number']").setValue("1234567890");
+        $("[data-qa='create-account']").click();
+        $("[data-qa='account-created']").shouldBe(visible);
     }
 }
--- src/test/java/Valid.java
+++ src/test/java/Valid.java
@@ -1,4 +1,2 @@
-package Registration;
-
 public class Valid {
 }
